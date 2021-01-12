be832d50e91d85909aca15c713203e1ebcfa532d
Joshua Miller <joshsmiller1022@gmail.com>
2021-01-11 20:55:09 -0700

    Complete except p6

--- higher-order.js
+++ higher-order.js
@@ -21,8 +21,8 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
-
+let evenNumbers = mixedNumbers.filter(num => num % 2 ===0)
+// console.log(evenNumbers)
 
 
 ////////// PROBLEM 2 //////////
@@ -44,7 +44,8 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(tax => tax *= 1.07)
+// console.log(postTaxPrices)
 
 
 
@@ -63,7 +64,10 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce(function(acc, curr){
+  return acc + curr
+})
+// console.log(totalPopulation)
 
 
 
@@ -83,13 +87,17 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 
 /*
   Now we're going to ramp these up a little bit.
-  Instead of just arrays of numbers, we are going to have array of objects that we want to use map, filter, and reduce with.
+  Instead of just arrays of numbers, we are going to have array of objects that we want to use map, filter, 
+  and reduce with.
 
   Use the filter method to return only the monsters that have a CP of over 200.
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(function(elem, ind, arr){
+  return elem.CP > 200
+})
+// console.log(myStrongest)
 
 
 
@@ -107,8 +115,10 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 */
 
 let orderTotals // Code here
-
-
+= orders.map(function(elem, ind, arr){
+  return elem.price * (1 + elem.tax)
+})
+// console.log(orderTotals)
 
 ////////// PROBLEM 6 //////////
 
@@ -127,5 +137,8 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
 */
 
 let bobsTotal //Code Here
-
+= purchases.reduce(function(acc, curr){
+  return acc + curr[owner, price]
+}, 0)
+console.log(bobsTotal)
 
